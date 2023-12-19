b60bc52dcf94dddc3683123b1377cb693d9c6510
Ben Walker <benwalker1.personal@gmail.com>
2023-12-20 09:55:13 +1300

    Added "ChosenPropertyListing" route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import {Routes,Route} from 'react-router-dom';
 import DataEntry from "./data-entry/DataEntry";
+import Chosenpropertylisting from "./pages/ben-walker/ChosenPropertyListing";
 
 function App() {
   return (
     <Routes>
       <Route path='/dataentry' element={<DataEntry />}></Route>
+      <Route path='/chosenpropertylisting' element={<Chosenpropertylisting />}></Route>
     </Routes>
   )
 }
