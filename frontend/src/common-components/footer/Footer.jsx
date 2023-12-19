0289435e6adb4a19c03dd12f4b4f769bcea7b427
Ben Walker <benwalker1.personal@gmail.com>
2023-12-20 09:34:05 +1300

    Added code to Footer component and corresponding CSS file. Style will change

--- frontend/src/common-components/footer/Footer.jsx
+++ frontend/src/common-components/footer/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons"; // Import the regular copyright icon
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={Styles.footer}>
+      <p>
+        <FontAwesomeIcon icon={faCopyright} className={Styles.copyRightIcon} />
+        LevelUp Works 2020
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
