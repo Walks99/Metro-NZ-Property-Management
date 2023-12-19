605d5a324f6a13f6ae7da983b34b3a88ce7e27be
Ben Walker <benwalker1.personal@gmail.com>
2023-12-20 09:59:40 +1300

    Added Route to Footer in the "ChosenPropertyListing" page

--- frontend/src/common-components/footer/Footer.jsx
+++ frontend/src/common-components/footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Styles from "./Footer.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopyright } from "@fortawesome/free-regular-svg-icons"; // Import the regular copyright icon
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCopyright } from "@fortawesome/free-regular-svg-icons"; // Import the regular copyright icon
 
 function Footer() {
   return (
     <div className={Styles.footer}>
       <p>
-        <FontAwesomeIcon icon={faCopyright} className={Styles.copyRightIcon} />
-        LevelUp Works 2020
+        {/* <FontAwesomeIcon icon={faCopyright} className={Styles.copyRightIcon} /> */}
+        Footer
       </p>
     </div>
   );
--- frontend/src/pages/ben-walker/ChosenPropertyListing.jsx
+++ frontend/src/pages/ben-walker/ChosenPropertyListing.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from "../../common-components/footer/Footer"
 
 function ChosenPropertyListing() {
   return (
+    <>
     <div>ChosenPropertyListing</div>
+    <Footer />
+    </>
   )
 }
 
