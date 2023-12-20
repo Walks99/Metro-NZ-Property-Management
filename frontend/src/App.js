f8f28116f8854fe72f42f907ce41633d77910e54
Ariel Joseph Amano <arieljosephamano@gmailcom>
2023-12-20 18:42:08 +1300

    integrated ariel component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import DataEntry from "./data-entry/DataEntry";
 import Chosenpropertylisting from "./pages/ben-walker/ChosenPropertyListing";
+import PropertyListing from "./pages/propertyListing/PropertyListing";
 
 function App() {
   return (
     <Routes>
-      <Route path='/dataentry' element={<DataEntry />}></Route>
-      <Route path='/chosenpropertylisting' element={<Chosenpropertylisting />}></Route>
+      <Route path="/dataentry" element={<DataEntry />}></Route>
+      <Route path="/propertyListing" element={<PropertyListing />}></Route>
+      <Route
+        path="/chosenpropertylisting"
+        element={<Chosenpropertylisting />}
+      ></Route>
     </Routes>
-  )
+  );
 }
 
 export default App;
