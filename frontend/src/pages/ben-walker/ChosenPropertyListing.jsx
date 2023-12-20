294191bd5ca1fff0093d8905b424a8926eeda7d5
Ben Walker <benwalker1.personal@gmail.com>
2023-12-20 18:26:18 +1300

    Commiting in progress work on proerty listings page

--- frontend/src/pages/ben-walker/ChosenPropertyListing.jsx
+++ frontend/src/pages/ben-walker/ChosenPropertyListing.jsx
@@ -7,7 +7,14 @@ function ChosenPropertyListing() {
   return (
     <div className={Styles.chosenPropertyListingContainer}>
       <Navbar />
-      <p>Chosen Property listings page</p>
+      <propertyimages className={Styles.propertyImagesContainer}>
+          <p style={{color: "red"}} className={Styles.mainImageContainer}><img className={Styles.mainImage}src={process.env.PUBLIC_URL + "/images/3-amano-avenue-epsom-auckland/2081897491.jpg"} alt="Main of property"/></p>
+          <p style={{color: "red"}} className={Styles.additionalImageContainer}><img className={Styles.additionalImage} src={process.env.PUBLIC_URL + "/images/3-amano-avenue-epsom-auckland/2081897497.jpg"} alt="Main of property"/></p>
+          <p style={{color: "red"}} className={Styles.additionalImageContainer}><img className={Styles.additionalImage} src={process.env.PUBLIC_URL + "/images/3-amano-avenue-epsom-auckland/2081897504.jpg"} alt="Main of property"/></p>
+          <p style={{color: "red"}} className={Styles.additionalImageContainer}><img className={Styles.additionalImage} src={process.env.PUBLIC_URL + "/images/3-amano-avenue-epsom-auckland/2081897505.jpg"} alt="Main of property"/></p>
+          <p style={{color: "red"}} className={Styles.additionalImageContainer}><img className={Styles.additionalImage} src={process.env.PUBLIC_URL + "/images/3-amano-avenue-epsom-auckland/2081897507.jpg"} alt="Main of property"/></p>
+
+      </propertyimages>
       <Footer />
     </div>
   );
