c39ad589fc92c0cd741f3af8364e53c6cd9e1bab
Ben Walker <benwalker1.personal@gmail.com>
2023-12-20 13:58:11 +1300

    Commiting completed Foor component

--- frontend/src/common-components/footer/Footer.jsx
+++ frontend/src/common-components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from 'react-router-dom';
 import Styles from "./Footer.module.scss";
 import InstagramIcon from "../../assets/icons/Instagram.png";
 import TwitterIcon from "../../assets/icons/Twitter.png";
@@ -11,33 +12,32 @@ function Footer() {
         <topfootersection className={Styles.topFooterContainer}>
 {/* --------------------------------   TITLE OF FOOTER   ------------------------------------- */}
           <footertitle className={Styles.footerTitleContainer}>
-            <h2 className={Styles.quicklinks}>Quick Links</h2>
+            <h2>Quick Links</h2>
             <h2>Follow Us</h2>
           </footertitle>
 {/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */}
 {/* -----------------------   QUICKS LINKS AND ICONS CONTAINER   ----------------------------- */}
           <quicklinksandicons className={Styles.quickLinksAndIconsContainer}>
-            <quicklinks>
-              <p>Home</p>
-              <p>Tenants</p>
-              <p>About</p>
-              <p>Property Listings</p>
-              <p>We're Hiring</p>
-              <p>News</p>
-              <p>Services</p>
-              <p>Contact</p>
-              <p>Dispute Process</p>
+            <quicklinks className={Styles.quickLinks}>
+              <NavLink to="/"><span>Home</span></NavLink>
+              <NavLink to="/tenants"><span>Tenants</span></NavLink>
+              <NavLink to="/about"><span>About</span></NavLink>
+              <NavLink to="/property-listings"><span>Property Listings</span></NavLink>
+              <NavLink to="/we-are-hiring"><span>We're Hiring</span></NavLink>
+              <NavLink to="/news"><span>News</span></NavLink>
+              <NavLink to="/services"><span>Services</span></NavLink>
+              <NavLink to="/contact"><span>Contact</span></NavLink>
+              <NavLink to="/dispute-process"><span>Dispute Process</span></NavLink>
             </quicklinks>
-            <icons>
-              <img src={InstagramIcon} alt="Heart Icon" />
-              <img src={TwitterIcon} alt="Heart Icon" />
-              <img src={FacebookIcon} alt="Heart Icon" />
-              <img src={LinkedinIcon} alt="Heart Icon" />
+            <icons className={Styles.icons}>
+              <a href="https://www.instagram.com/metronzproperty" target="_blank" rel="noopener noreferrer"><img src={InstagramIcon} alt="Instagram icon" /></a>
+              <a href="https://twitter.com/MetroNz_" target="_blank" rel="noopener noreferrer"><img src={TwitterIcon} alt="X (formally Twitter) icon" /></a>
+              <a href="https://www.facebook.com/MetroNZ/" target="_blank" rel="noopener noreferrer"><img src={FacebookIcon} alt="Facebook icon" /></a>
+              <a href="https://www.linkedin.com/company/metro-nz-property-management-ltd" target="_blank" rel="noopener noreferrer"><img src={LinkedinIcon} alt="LinkedIn icon" /></a>
             </icons>
           </quicklinksandicons>
 {/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */}
       </topfootersection>
-      
       <bottomfootersection className={Styles.bottomFooterContainer}>
 {/* -------------------------------   COPY RIGHT NOTICE   ------------------------------------ */}
         <copyrightnotice className={Styles.copyRightNoticeContainer}>
